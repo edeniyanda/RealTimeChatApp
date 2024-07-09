43685059b1aa04f87d6e6438d2da81df7967d7b8
Eden Iyanda <edeniyanda@gmail.com>
2024-07-09 12:24:42 +0100

    Update: Added template to folder

--- chating/urls.py
+++ chating/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from personal.views import (home_page)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page, name="home_page"),
+
 ]
