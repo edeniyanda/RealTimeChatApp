c001645de26349104de75715e3fc5ae6a038b722
Eden Iyanda <edeniyanda@gmail.com>
2024-07-09 13:09:28 +0100

    Added static and media file settings and URLs configuration

--- chating/urls.py
+++ chating/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from personal.views import (home_page)
 
 urlpatterns = [
@@ -23,3 +25,7 @@ urlpatterns = [
     path('', home_page, name="home_page"),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
